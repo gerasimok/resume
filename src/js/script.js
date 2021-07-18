c7c33ee93e3a7744f2858459b098147692211a05
Dmitriy <jackassjn@gmail.com>
2021-07-18 16:59:41 +0300

    add addaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,37 @@
+/*jshint esversion: 6 */
+
 const 	hamburger = document.querySelector('.hamburger'),
 		menu = document.querySelector('.menu'),
 		closeElem = document.querySelector('.menu__close');
 
-hamburger.addEventListener('click', () =>{
-	menu.classList.add('active');
-});
+hamburger.addEventListener('click', () => {
+		menu.classList.add('active');
+	});
 
-closeElem.addEventListener('click', () =>{
-	menu.classList.remove('active');
-});
+closeElem.addEventListener('click', () => {
+		menu.classList.remove('active');
+	});
 
 const 	counters  = document.querySelectorAll('.skills__ratings-counter'),
 	  	lines = document.querySelectorAll('.skills__ratings-line span');
-
 counters.forEach( (item, i) => {
 	lines[i].style.width = item.innerHTML;
+});
+
+
+$(document).ready(function(){
+	$('form').submit(function(e)  {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thaks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
